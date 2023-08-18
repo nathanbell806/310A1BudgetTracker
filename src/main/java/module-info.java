5d591dcbbcd63b7f55b3b6d46c8830c696bf0498
tmur647 <tmur647@aucklanduni.ac.nz>
2023-08-18 19:51:12 +1200

    added tests for the profilerepository and currentProfile singleton

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,7 +7,13 @@ module com.example.budgettracker {
   requires org.kordamp.ikonli.javafx;
   requires org.kordamp.bootstrapfx.core;
   requires com.google.gson;
+  requires junit;
 
-  opens com.example.budgettracker to javafx.fxml;
+  opens com.example.budgettracker to javafx.fxml, com.google.gson;
   exports com.example.budgettracker;
+  exports com.example.budgettracker.profiles;
+  opens com.example.budgettracker.profiles to javafx.fxml,com.google.gson;
+
+  opens tests to javafx.fxml,com.google.gson ;
+  exports tests;
 }
---
+++ src/main/java/tests/ProfileTests.java
@@ -0,0 +1,52 @@
+package tests;
+import com.example.budgettracker.profiles.CurrentProfile;
+import com.example.budgettracker.profiles.ProfileFactory;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.Assert.*;
+
+public class ProfileTests {
+    ProfileFactory profileFactory;
+
+    CurrentProfile currentProfile;
+
+    @Before
+    public void setup() throws IOException {
+        profileFactory = new ProfileFactory();
+        currentProfile = CurrentProfile.getInstance();
+    }
+
+    @Test
+    public void createProfile() throws IOException {
+        //make sure that slot 2 username is empty
+        boolean result1 = profileFactory.createProfile("user4","black",2);
+        assertTrue(result1);
+        //make sure slot 3 has a user called user5
+        boolean result2 = profileFactory.createProfile("user5", "black", 3);
+        assertFalse(result2);
+    }
+
+    @Test
+    public void testSingletonAndSelect() {
+        currentProfile.setCurrentProfile(profileFactory.selectProfile("user5"));
+        currentProfile.setProfileSlot(3);
+
+        int userSlot = currentProfile.getProfileSlot();
+        assertEquals(3, userSlot);
+
+
+    }
+
+    @Test
+    public void testSaveAndGetALlProfiles() throws IOException {
+        //set the third slot color to black
+        currentProfile.getCurrentProfile().setColor("orange");
+        profileFactory.saveProfile(currentProfile.getCurrentProfile());
+        assertEquals("orange",profileFactory.getAllProfiles().get(2).getColor());
+
+
+    }
+}
