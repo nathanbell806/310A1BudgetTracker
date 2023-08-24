f94e0a4498568889c238fa08403746af8749f242
Yxin-KaiByz <100540976+Yxin-KaiByz@users.noreply.github.com>
2023-08-25 09:04:07 +1200

    Added fxml for budget item

--- src/main/java/com/example/budgettracker/BudgetCategoriesController.java
+++ src/main/java/com/example/budgettracker/BudgetCategoriesController.java
@@ -1,11 +1,13 @@
 package com.example.budgettracker;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 
 public class BudgetCategoriesController {
     @FXML private Label leftBudgetLabel;
@@ -16,6 +18,8 @@ public class BudgetCategoriesController {
     @FXML private Button finishAddBtn;
     @FXML private StackPane overlay;
     @FXML private TextField categoryNameField;
+    @FXML private TextField budgetedValueField;
+    @FXML private VBox categoryList;
 
 
     @FXML
@@ -45,6 +49,12 @@ public class BudgetCategoriesController {
     @FXML
     public void onFinishAddCategory(){
         //TODO: save info and add list item
+        String catName = categoryNameField.getText();
+        String budgetedValue = budgetedValueField.getText();
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("categoryItem"));
+
+        categoryList.getChildren().add(0, addCategoryBtn);
         popupPane.setVisible(false);
         popupPane.setDisable(true);
         overlay.setDisable(true);
--- src/main/java/com/example/budgettracker/controller/CategoryItemController.java
+++ src/main/java/com/example/budgettracker/controller/CategoryItemController.java
@@ -1,5 +1,32 @@
 package com.example.budgettracker.controller;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class CategoryItemController {
-    
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class CategoryItemController implements Initializable{
+        @FXML
+    private Label budgetedValue;
+
+    @FXML
+    private Label categoryName;
+
+    @FXML
+    private Button deleteBtn;
+
+    @FXML
+    private Button editBtn;
+
+    public void setData(String budgetedValueData, String categoryNameData){
+        categoryName.setText(categoryNameData);
+        budgetedValue.setText(budgetedValueData);
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // TODO Auto-generated method stub
+    }
 }
