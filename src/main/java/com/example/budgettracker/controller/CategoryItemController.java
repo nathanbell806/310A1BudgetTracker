b59b21af16cdb2909504c4373857e66133e5b5a9
Yxin-KaiByz <100540976+Yxin-KaiByz@users.noreply.github.com>
2023-08-25 08:33:30 +1200

    Added sample categories and default layout for category item

---
+++ src/main/java/com/example/budgettracker/controller/CategoryItemController.java
@@ -0,0 +1,5 @@
+package com.example.budgettracker.controller;
+
+public class CategoryItemController {
+    
+}
---
+++ src/main/java/com/example/budgettracker/controller/CategoryListController.java
@@ -0,0 +1,16 @@
+package com.example.budgettracker.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+
+public class CategoryListController {
+
+
+    @FXML
+    private VBox categoryList;
+
+    @FXML
+    private TextField categoryNameField;
+
+}
