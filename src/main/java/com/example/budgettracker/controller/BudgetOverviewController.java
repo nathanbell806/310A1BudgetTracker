f06777abf0803e499262d4cced15c251b155559a
ejiang25 <ejiang25@gmail.com>
2023-09-21 19:53:48 +1200

    added savings forecast and hover effects for chart points

--- src/main/java/com/example/budgettracker/controller/BudgetOverviewController.java
+++ src/main/java/com/example/budgettracker/controller/BudgetOverviewController.java
@@ -11,8 +11,15 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.chart.LineChart;
 import javafx.scene.chart.PieChart;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Tooltip;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 
 public class BudgetOverviewController {
   @FXML
@@ -24,6 +31,12 @@ public class BudgetOverviewController {
   @FXML
   private Label totalText;
 
+  @FXML
+  private LineChart<Number, Number> lineChart;
+
+  @FXML
+  private Button toggleButton;
+
   private String username;
 
   private double totalBudget;
@@ -34,6 +47,7 @@ public class BudgetOverviewController {
    */
   private void dataGet() {
     Profile profile = CurrentProfile.getInstance().getCurrentProfile();
+    XYChart.Series<Number, Number> series = new XYChart.Series<>();
     budgetData = FXCollections.observableArrayList();
 
     totalBudget = profile.getBudget();
@@ -48,6 +62,14 @@ public class BudgetOverviewController {
     if(totalBudgetLeft > 0){
       budgetData.add(new PieChart.Data("Savings", totalBudgetLeft));
     }
+
+
+    series.setName("Your Current Spending");
+    // Show savings for 10 years
+    for (int i = 0; i < 11; i++) {
+      series.getData().add(new XYChart.Data<>(i, (totalBudgetLeft + profile.getSavings()) * i));
+    }
+    lineChart.getData().add(series);
   }
 
   /**
@@ -58,9 +80,56 @@ public class BudgetOverviewController {
     dataGet();
 
     usernameText.setText(username);
-    totalText.setText("Total budget is " + totalBudget);
+    totalText.setText("Total budget is $" + totalBudget);
     pieChart.setData(budgetData);
+    addEventHandlersCharts();
+    // Handle the toggle button click event
+    toggleButton.setOnAction(event -> {
+      // Toggle visibility of charts
+      pieChart.setVisible(!pieChart.isVisible());
+      lineChart.setVisible(!lineChart.isVisible());
+    });
+  }
+
+  /**
+   * Sets title and mouse events for charts
+   */
+  public void addEventHandlersCharts(){
     pieChart.setTitle("Budget Breakdown Pie Chart");
+    // Add event handlers to each pie chart slice
+    Tooltip tooltip = new Tooltip();
+    tooltip.setStyle("-fx-font-size: 20");
+    for (PieChart.Data data : pieChart.getData()) {
+      data.getNode().addEventHandler(MouseEvent.MOUSE_ENTERED, (MouseEvent e) -> {
+        if (!tooltip.isShowing()) {
+          // Set the tooltip text to display the value when the mouse enters the slice
+          tooltip.setText("$" + String.valueOf(data.getPieValue()));
+          tooltip.show(data.getNode(), e.getScreenX() + 10, e.getScreenY() + 10);
+        }
+      });
+      data.getNode().addEventHandler(MouseEvent.MOUSE_EXITED, (MouseEvent e) -> {
+        // Hide the tooltip when the mouse exits the slice
+        tooltip.hide();
+      });
+    }
+    // Add event handlers to each data series in the LineChart
+    for (XYChart.Series<Number, Number> series : lineChart.getData()) {
+      for (XYChart.Data<Number, Number> data : series.getData()) {
+        data.getNode().addEventHandler(MouseEvent.MOUSE_ENTERED, (MouseEvent e) -> {
+          // Set the tooltip text to display the value when the mouse enters the data point
+          String tooltipText = "$" + data.getYValue();
+          tooltip.setText(tooltipText);
+
+          // Show the tooltip immediately
+          tooltip.show(data.getNode(), e.getScreenX() + 10, e.getScreenY() + 10);
+        });
+
+        data.getNode().addEventHandler(MouseEvent.MOUSE_EXITED, (MouseEvent e) -> {
+          // Hide the tooltip when the mouse exits the data point
+          tooltip.hide();
+        });
+      }
+    }
   }
 
   /**
